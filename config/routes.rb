5b5f978ff6c948e75e9fdfb7ac3261f63463addd
Alexander Yuen <alexandertyuen@gmail.com>
2016-12-02 07:17:36 +0000

    installed devise gem for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
     root to: 'pages#home'
     get '/about', to: 'pages#about'
     resources :contacts, only: :create # Don't need to add 'new' since we made custom URL for it below [:new, :create]
