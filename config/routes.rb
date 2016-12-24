e8d2fb9daf1bc81877c8cef1030df3257e8a6084
Alexander Yuen <alexandertyuen@gmail.com>
2016-12-24 23:53:09 +0000

    root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root 'pages#home'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get '/about', to: 'pages#about'
   resources :contacts, only: :create # Don't need to add 'new' since we made custom URL for it below [:new, :create]
