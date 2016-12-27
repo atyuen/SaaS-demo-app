a14b103b811f2c4549e8782eb118efa9d706395e
Alexander Yuen <alexandertyuen@gmail.com>
2016-12-27 23:20:40 +0000

    added file field for image upload to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,6 +22,6 @@ class ProfilesController < ApplicationController
     
     private
         def profile_params
-           params.require(:profile).permit(:first_name, :last_name, :title, :phone_number, :contact_email, :description) 
+           params.require(:profile).permit(:first_name, :last_name, :avatar, :title, :phone_number, :contact_email, :description) 
         end
 end
