4fd53bc5592da5c96ea8a35a6df01c01f2ddf087
Alexander Yuen <alexandertyuen@gmail.com>
2016-12-28 12:45:03 +0000

    ui overhaul

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
     def title_icon
-        if @user.profile.title == "Pet"
-            "<i class= 'fa fa-gittip'></i>".html_safe
-        elsif @user.profile.title == "Prospective Owner"
-            "<i class= 'fa fa-smile-o'></i>".html_safe
+        if @user.profile.title == "Developer"
+            "<i class= 'fa fa-code-fork'></i>".html_safe
+        elsif @user.profile.title == "Entrepreneur"
+            "<i class= 'fa fa-suitcase'></i>".html_safe
+        elsif @user.profile.title == "Investor"
+            "<i class= 'fa fa-dollar'></i>".html_safe
         end
     end
 end
